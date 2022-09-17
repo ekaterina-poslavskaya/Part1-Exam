2b760bd30f63ecf5f62640ddc936901c14648829
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-09-17 09:32:35 +0300

    Task: выбор из массива элементов с длиной <= 3

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Написать программу, которая из имеющегося массива 
+формирует массив из строк, длина которых мельше либо равна 
+3символа.
+Первоначальный массив можно ввести с клавиатуры, либо задать
+на старте выполнения алгоритма. При решении не рекомендуется
+пользоваться коллекциями, лучше обойтись исключительно 
+массивами.
+
+Примеры:
+["hello", "2", "world", ":"] -> ["2", ":-)"]
+*/
+
+public class clsArraySubSet
+{
+    static void PrintStringAray(string[] arr, string message = "")
+    {
+        if (message != "") Console.WriteLine(message);
+        if (arr.Length > 0)
+        {
+            Console.Write($"['{arr[0]}'");
+            for (int i = 1; i < arr.Length; i++)
+                Console.Write($", '{arr[i]}'");
+            Console.WriteLine("]");
+        }
+        else
+            Console.WriteLine("Массив нулевой длины!");
+    }
+
+    static int GetCountOfStrings(string[] arr, int controlStringLength)
+    {
+        int resCount = 0;
+        if ((controlStringLength > 0) && (arr.Length > 0))
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length <= controlStringLength)
+                    resCount++;
+            }
+        }
+        return resCount;
+    }
+
+    static void CopyStrings(string[] initArr, string[] resArr, int controlStringLength)
+    {
+        int resArrayIndex = 0;
+        if ((controlStringLength > 0) && (initArr.Length > 0) && (resArr.Length > 0))
+        {
+            for (int i = 0; i < initArr.Length; i++)
+            {
+                if (initArr[i].Length <= controlStringLength)
+                {
+                    resArr[resArrayIndex] = initArr[i];
+                    resArrayIndex++;
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        //Несколько тестовых наборов
+        // задаем их переменной strArray, которая далее везде используется
+        string[] strArray1 = { "12345", "1456", "-2", "hv", " " };
+        string[] strArray2 = {};
+        string[] strArray3 = {"hgdd", "dsfdf", "hsf:dsf", "00000", "     "};
+
+
+
+        string[] strArray = strArray3;
+        string[] resArray = new string[GetCountOfStrings(strArray, 3)];
+
+        CopyStrings(strArray, resArray, 3);
+
+        PrintStringAray(strArray, "Исходный массив:");
+        PrintStringAray(resArray, "Результирующий массив:");
+    }
+}
